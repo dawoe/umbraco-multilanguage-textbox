01ea5f50371c305eae18a8b5ca3a68636d8ba971
Dave Woestenborghs <dave.woestenborghs@dotcontrol.com>
2022-07-08 18:27:13 +0200

    Update scripts to start

--- Utilities/StartTestSite.ps1
+++ Utilities/StartTestSite.ps1
@@ -1,6 +1,6 @@
 ﻿$CurrentDir = Split-Path $MyInvocation.MyCommand.Path
 $RootDir = Split-Path -Path $CurrentDir -Parent
-$SiteDir= "$RootDir\testsites\v9"
+$SiteDir= "$RootDir\testsites\MultiLanguageTextbox"
 
 
 
