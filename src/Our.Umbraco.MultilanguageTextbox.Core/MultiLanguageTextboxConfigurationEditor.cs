e13bf2b737f1e6b8a41b81f5ec41416d42bda554
Dave Woestenborghs <dave.woestenborghs@dotcontrol.com>
2022-07-08 18:06:08 +0200

    Make solution only target v10

--- src/Our.Umbraco.MultilanguageTextbox.Core/MultiLanguageTextboxConfigurationEditor.cs
+++ src/Our.Umbraco.MultilanguageTextbox.Core/MultiLanguageTextboxConfigurationEditor.cs
@@ -2,12 +2,8 @@
 // Copyright (c) Dave Woestenborghs &amp; Contributors
 // </copyright>
 
-#if NET472
-using Umbraco.Core.PropertyEditors;
-#else
 using Umbraco.Cms.Core.IO;
 using Umbraco.Cms.Core.PropertyEditors;
-#endif
 
 namespace Our.Umbraco.MultilanguageTextbox.Core
 {
@@ -16,7 +12,6 @@ namespace Our.Umbraco.MultilanguageTextbox.Core
     /// </summary>
     internal class MultiLanguageTextboxConfigurationEditor : ConfigurationEditor<MultiLanguageTextboxConfiguration>
     {
-#if NETCOREAPP
         /// <summary>
         /// Initializes a new instance of the <see cref="MultiLanguageTextboxConfigurationEditor"/> class.
         /// </summary>
@@ -25,7 +20,6 @@ namespace Our.Umbraco.MultilanguageTextbox.Core
             : base(ioHelper)
         {
         }
-#endif
 
         /// <inheritdoc/>
         public override object DefaultConfigurationObject => new MultiLanguageTextboxConfiguration
